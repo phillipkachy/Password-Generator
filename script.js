8a767da1f54f3f4c23593ccf7b45edc93e779cfd
phillipkachy <phillipkachy@gmail.com>
2022-03-11 10:26:15 -0500

    Creatin of functions for random characters

--- script.js
+++ script.js
@@ -12,6 +12,31 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Creating prompt
+function generatePassword() {
+  return prompt('Hello');
+}
+// lower case
+function generateLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// upper case
+function generateUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// random number
+function generateNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// random symbol
+function generateSymbol() {
+  const symbols = '!@#$%^&*';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(generatePassword());
+console.log(generateLower() + generateUpper() + generateNumber() + generateSymbol());
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
