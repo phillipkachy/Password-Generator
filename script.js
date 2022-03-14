d8c4e897ba4b746558beeee278c4b7f9cebe4941
phillipkachy <phillipkachy@gmail.com>
2022-03-13 22:32:16 -0400

    add of character variables + prompt function

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 console.log('you clicked the button');
 
+var specialChar = ['!','@','#','$','%','^','&','*','<','>','(',')'];
+var lowerCase = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
+var upperCase = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
+var number = ['1','2','3','4','5','6','7','8','9','0'];
+var charLenght = 8;
 //Prompt the user
-//Password lenght
-//Lowercase, Uppercase, Numbers, Symbols
+function getPrompts() {
+charLenght = parseInt(prompt('Insert password length'));
+
+if(isNaN(charLenght));
+}
 //Display password
 
 return'Generated password will go here!'
